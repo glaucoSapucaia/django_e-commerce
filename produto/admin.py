0ee6ab4e1e4548e1d0d4c4ca8d3f7f9d04d0f9a1
glaucoSapucaia <glaucopco@gmail.com>
2023-12-19 20:55:51 -0300

    Create requirements.txt | Update .gitignore | Create and register models Produto and Varaiação | Create Variação (TabularInline) | Create utils and resize_image function

--- produto/admin.py
+++ produto/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from produto.models import Produto, Variacao
+
+# more details
+class VariacaoInLine(admin.TabularInline):
+    model = Variacao
+    extra = 1
 
 # Register your models here.
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'nome', 'slug', 'preco_marketing', 'tipo', )
+    list_display_links = ('nome', )
+    list_per_page = 9
+    search_fields = ('pk', 'nome', 'preco_marketing', 'tipo', )
+    inlines = (
+        VariacaoInLine,
+    )
+
+@admin.register(Variacao)
+class VariacaoAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'produto', 'nome', 'preco', 'estoque', )
+    list_display_links = ('produto', 'nome')
+    list_per_page = 9
+    search_fields = ('pk', 'produto', 'nome', 'preco', 'estoque', )
--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from utils.resize_image import resizeImage
 
 # Create your models here.
+class Produto(models.Model):
+    class Meta:
+        verbose_name = 'Produto'
+        verbose_name_plural = 'Produtos'
+    
+    nome = models.CharField(max_length=255)
+    descricao_curta = models.TextField(max_length=255)
+    descricao_longa =models.TextField()
+    imagem = models.ImageField(upload_to='produto_imagens/%Y/%m', blank=True, null=True)
+    slug = models.SlugField(unique=True)
+    preco_marketing = models.FloatField()
+    preco_marketing_promocional = models.FloatField(default=0)
+    tipo = models.CharField(
+        default='V',
+        max_length=1,
+        choices = (
+            ('V', 'Variação'),
+            ('S', 'Simples'),
+        ),
+    )
+
+    def __str__(self) -> str:
+        return self.nome
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if self.imagem:
+            resizeImage(self.imagem)
+
+class Variacao(models.Model):
+    class Meta:
+        verbose_name = 'Variação'
+        verbose_name_plural = 'Variações'
+
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=50, blank=True, null=True)
+    preco = models.FloatField()
+    preco_promocinal = models.FloatField(default=0)
+    estoque = models.PositiveIntegerField(default=1)
+
+    def __str__(self) -> str:
+        return self.nome or self.produto.nome
