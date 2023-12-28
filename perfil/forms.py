8865929e255a91c09a0e9ae29a88219bdd23d354
glaucoSapucaia <glaucopco@gmail.com>
2023-12-28 17:49:01 -0300

    Update UserForm logic pt1

--- perfil/forms.py
+++ perfil/forms.py
@@ -1,4 +1,3 @@
-from typing import Any
 from django.contrib.auth.models import User
 from django import forms
 from perfil import models
@@ -10,7 +9,12 @@ class PerfilForm(forms.ModelForm):
         exclude = ('usuario',)
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(
+    password1 = forms.CharField(
+        required=False,
+        widget=forms.PasswordInput(),
+        label='Confirmação da senha',
+    )
+    password2 = forms.CharField(
         required=False,
         widget=forms.PasswordInput(),
         label='Senha',
@@ -23,9 +27,46 @@ class UserForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'username', 'email', 'password')
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
 
-    def clean(self) -> dict[str, Any]:
+    def clean(self):
         data = self.data
         cleaned = self.cleaned_data
-        return super().clean()
+        validation_error_msgs = {}
+
+        user_data = cleaned.get('username')
+        email_data = cleaned.get('email')
+        password_data = cleaned.get('password')
+        password2_data = cleaned.get('password2')
+
+        user_db = User.objects.filter(username=user_data).first()
+        email_db = User.objects.filter(email=email_data).first()
+
+        error_msg_user_exists = 'Usuário já cadastrado'
+        error_msg_email_exists = 'Email já cadastrado'
+        error_msg_password_match = 'As senhas precisam ser iguais'
+        error_msg_password_short = 'A senha precisa ter pelo menos seis caracteres'
+
+        if self.usuario:
+            if user_data != user_db.username:
+                if user_db:
+                    validation_error_msgs['username'] = error_msg_user_exists
+
+            if email_data != email_db.email:
+                if email_db:
+                    validation_error_msgs['email'] = error_msg_email_exists
+
+            if password_data:
+                if password_data != password2_data:
+                    validation_error_msgs['password1'] = error_msg_password_match
+                    validation_error_msgs['password2'] = error_msg_password_match
+
+                if len(password_data) < 6:
+                    validation_error_msgs['password1'] = error_msg_password_short
+        else:
+            pass
+
+        if validation_error_msgs:
+            raise forms.ValidationError(
+                validation_error_msgs
+            )
